3b832d92640749c57fa5a1c3f646ef21b131fdb5
Andy Abebaw <andyabebaw1@gmail.com>
2022-12-16 14:01:11 -0800

    add weather basic functionality with hard coded coordinates

--- Assets/javascript/scripts.js
+++ Assets/javascript/scripts.js
@@ -1,73 +1,114 @@
+var currentWeather;
+var forecastData;
+
 var currentLocation = "Chicago";
-var cards = $(".card").map(function() {
+var long = 41.87;
+var lat = 87.62;
+
+
+var cards = $(".card").map(function () {
   return this;
 });
 
-
-
-
 $(function () {
-  setDates();
-  setTodaysWeather()
+ 
+  setTodaysDateTemp()
+  setWeatherForecast(long, lat)
+  getWeather(long, lat);
 });
 
 function getDate(daysinFuture) {
-  
+
   var today = new Date();
   var newDay = tomorrow = new Date(today)
+
   newDay.setDate(tomorrow.getDate() + daysinFuture)
+
   var dd = newDay.getDate();
-  
-  var mm = newDay.getMonth()+1; 
+  var mm = newDay.getMonth() + 1;
   var yyyy = newDay.getFullYear();
 
-  var date = mm+'/'+dd+'/'+yyyy;
+  var date = mm + '/' + dd + '/' + yyyy;
+
   return date;
 }
 
-
-
-function setDates(){
+function setTodaysDateTemp() {
   var dateEl = $("#locationDate");
   dateEl.text(currentLocation + " " + getDate(0));
-
-  $(".card").children(".weatherDate")
-  // console.log($(".card").children(".weatherDate"))
-  // console.log(cards.children(".weatherDate"))
-  for (var i = 0; i <cards.length; i++){
-    var weatherDate = cards.children(".weatherDate");
-    weatherDate[i].innerHTML = (getDate(i+1));
-    
-    var temp= cards.children(".temp");
-    temp[i].innerHTML = "Temp: " + i;
-
-    var wind= cards.children(".wind");
-    wind[i].innerHTML = "Wind: " + i;
-
-    var humidity= cards.children(".humidity");
-    humidity[i].innerHTML = "Humidity: " + i;
-  }
-  
 }
 
-function setTodaysWeather(){
-  var weatherJSON = getWeather(41.87,87.62)
-  $("#todaysTemp").text()
-  $("#todaysWind").text()
-  $("#todaysHumidity").text()
-}
-
-
-function getWeather(long, lat){
-  var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=ee5bac9a1e0c0e170057e26226f0931a"
+function setWeatherForecast(long, lat) {
+ var weatherURL = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + long + "&appid=ee5bac9a1e0c0e170057e26226f0931a&units=imperial";
   fetch(weatherURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)})
+      forecastData = data;
+      setForecastData(long,lat);
+      console.log(data);
+    })
 }
 
+function setForecastData(long, lat) {
+
+
+  $(".card").children(".weatherDate")
+  for (var i = 0; i < cards.length; i++) {
+    var weatherDate = cards.children(".weatherDate");
+    weatherDate[i].innerHTML = (getDate(i + 1));
+
+    var day = forecastData.daily[i];
+
+    var icon = cards.children(".emoji");
+    var iconurl = "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
+    icon.attr('src', iconurl);
+    console.log(iconurl)
+    var temp = cards.children(".temp");
+    temp[i].innerHTML = "Temp: " + day.temp.day + " °F";
+
+    var wind = cards.children(".wind");
+    wind[i].innerHTML = "Wind: " + day.wind_speed + " MPH";
+
+    var humidity = cards.children(".humidity");
+    humidity[i].innerHTML = "Humidity: " + day.humidity + "%";
+  }
+
+}
+
+function setTodaysTemp() {
+  var temp = $("#todaysTemp");
+  temp.text(currentWeather.main.temp);
+}
+
+function setTodaysWindSpeed() {
+  var wind = $("#todaysWind");
+  wind.text((currentWeather.wind.speed));
+}
+
+function setTodaysHumidity() {
+  var humidity = $("#todaysHumidity");
+  humidity.text((currentWeather.main.humidity));
+}
+
+
+
+
+function getWeather(long, lat) {
+  var weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=ee5bac9a1e0c0e170057e26226f0931a&units=imperial";
+  fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      currentWeather = data;
+      setTodaysTemp();
+      setTodaysWindSpeed();
+      setTodaysHumidity();
+      console.log(data);
+    })
+}
 
 
 
